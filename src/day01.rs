038f587562914d5f13d8d58e2a219c62376c53db
David M. Lee <leedm777@yahoo.com>
2021-11-30 23:27:24 -0600

    Day 1, part 2

--- src/day01.rs
+++ src/day01.rs
@@ -10,6 +10,11 @@ fn part1(input: Vec<i32>) -> i32 {
     return increases;
 }
 
+fn part2(input: Vec<i32>) -> i32 {
+    let avg = input.windows(3).map(|w| w.iter().sum()).collect();
+    return part1(avg);
+}
+
 #[cfg(test)]
 mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
@@ -2045,5 +2050,17 @@ mod tests {
     fn test_part1_real() {
         let actual = part1(real());
         assert_eq!(actual, 1688);
+    }
+
+    #[test]
+    fn test_part2_ex1() {
+        let actual = part2(ex1());
+        assert_eq!(actual, 5);
+    }
+
+    #[test]
+    fn test_part2_real() {
+        let actual = part2(real());
+        assert_eq!(actual, 1728);
     }
 }
