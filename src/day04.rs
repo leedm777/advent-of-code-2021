87cddfb9e678df14acf639dab04548aa4547941f
David M. Lee <leedm777@yahoo.com>
2021-12-04 00:35:42 -0600

    Day 4, part 1
    I'm totally not understanding Rust types yet. And I feel horrible with
    the mutable implementation I ended up with.

---
+++ src/day04.rs
@@ -0,0 +1,230 @@
+use crate::util;
+
+#[derive(Clone)]
+struct Board {
+    grid: [[i32; 5]; 5],
+    marked: [[bool; 5]; 5],
+}
+
+impl Board {
+    fn play(&mut self, n: i32) {
+        for row_number in 0..5 {
+            for col_number in 0..5 {
+                if self.grid[row_number][col_number] == n {
+                    self.marked[row_number][col_number] = true;
+                }
+            }
+        }
+    }
+
+    fn is_winner(&self) -> bool {
+        for row_number in 0..5 {
+            if self.marked[row_number] == [true; 5] {
+                return true;
+            }
+        }
+
+        'outer: for col_number in 0..5 {
+            for row_number in 0..5 {
+                if !self.marked[row_number][col_number] {
+                    break 'outer;
+                }
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+    fn score(&self) -> i32 {
+        let mut r = 0;
+
+        for col_number in 0..5 {
+            for row_number in 0..5 {
+                if !self.marked[row_number][col_number] {
+                    r += self.grid[row_number][col_number]
+                }
+            }
+        }
+
+        return r;
+    }
+}
+
+struct Game {
+    moves: Vec<i32>,
+    boards: Vec<Board>,
+}
+
+impl Game {
+    fn play(&mut self) -> i32 {
+        let n = self.moves.pop().unwrap();
+        for board in &mut self.boards {
+            board.play(n);
+        }
+        return n;
+    }
+
+    fn find_winner(&self) -> Option<&Board> {
+        return self.boards.iter().find(|board| board.is_winner());
+    }
+}
+
+fn parse_board(input: &[String]) -> Board {
+    let mut r = Board {
+        grid: [[0; 5]; 5],
+        marked: [[false; 5]; 5],
+    };
+
+    for row_number in 0..5 {
+        let mut row = input[row_number].split_whitespace();
+        for col_number in 0..5 {
+            let cell = row.next().unwrap().parse::<i32>().unwrap();
+            r.grid[row_number][col_number] = cell;
+        }
+    }
+
+    return r;
+}
+
+fn parse_game(input: &Vec<String>) -> Game {
+    let mut splits = input.split(|line| line.is_empty());
+    let mut moves: Vec<i32> = splits
+        .next()
+        .unwrap()
+        .first()
+        .unwrap()
+        .split(",")
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect();
+    moves.reverse();
+    let boards = splits;
+    return Game {
+        moves: moves,
+        boards: boards.map(|b| parse_board(b)).collect(),
+    };
+}
+
+pub fn part1(input: &Vec<String>) -> i32 {
+    let mut game = parse_game(input);
+
+    let mut n = game.play();
+    let mut winner = game.find_winner();
+
+    loop {
+        match winner {
+            Some(w) => {
+                return n * w.score();
+            }
+            None => {
+                n = game.play();
+                winner = game.find_winner();
+            }
+        }
+    }
+}
+
+pub fn part2(_input: &Vec<String>) -> i32 {
+    return 0;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn ex0() -> Vec<String> {
+        return vec![
+            "7,4,9,5",
+            "",
+            "22 13 17 11  0",
+            " 8  2 23  4 24",
+            "21  9 14 16  7",
+            " 6 10  3 18  5",
+            " 1 12 20 15 19",
+        ]
+        .into_iter()
+        .map(str::to_string)
+        .collect();
+    }
+
+    fn ex1() -> Vec<String> {
+        return vec![
+            "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1",
+            "",
+            "22 13 17 11  0",
+            " 8  2 23  4 24",
+            "21  9 14 16  7",
+            " 6 10  3 18  5",
+            " 1 12 20 15 19",
+            "",
+            " 3 15  0  2 22",
+            " 9 18 13 17  5",
+            "19  8  7 25 23",
+            "20 11 10 24  4",
+            "14 21 16 12  6",
+            "",
+            "14 21 17 24  4",
+            "10 16 15  9 19",
+            "18  8 23 26 20",
+            "22 11 13  6  5",
+            " 2  0 12  3  7",
+        ]
+        .into_iter()
+        .map(str::to_string)
+        .collect();
+    }
+
+    fn real() -> Vec<String> {
+        return util::file_as_strings("./src/day04.txt");
+    }
+
+    #[test]
+    fn test_parse_game() {
+        let actual = parse_game(&ex0());
+        let expected = Game {
+            moves: vec![5, 9, 4, 7], //[7, 4, 9, 5],
+            boards: vec![Board {
+                grid: [
+                    [22, 13, 17, 11, 0],
+                    [8, 2, 23, 4, 24],
+                    [21, 9, 14, 16, 7],
+                    [6, 10, 3, 18, 5],
+                    [1, 12, 20, 15, 19],
+                ],
+                marked: [
+                    [false, false, false, false, false],
+                    [false, false, false, false, false],
+                    [false, false, false, false, false],
+                    [false, false, false, false, false],
+                    [false, false, false, false, false],
+                ],
+            }],
+        };
+        assert_eq!(actual.moves, expected.moves);
+        assert_eq!(actual.boards[0].grid, expected.boards[0].grid);
+        assert_eq!(actual.boards[0].marked, expected.boards[0].marked);
+    }
+    #[test]
+    fn test_part1_ex1() {
+        let actual = part1(&ex1());
+        assert_eq!(actual, 4512);
+    }
+
+    #[test]
+    fn test_part1_real() {
+        let actual = part1(&real());
+        assert_eq!(actual, 55770);
+    }
+
+    #[test]
+    fn test_part2_ex1() {
+        let actual = part2(&ex1());
+        assert_eq!(actual, 0);
+    }
+
+    #[test]
+    fn test_part2_real() {
+        let actual = part2(&real());
+        assert_eq!(actual, 0);
+    }
+}
