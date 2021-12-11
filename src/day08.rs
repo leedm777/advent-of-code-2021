54cba11543654f8f8f50e654f5e4af79a6788c99
David M. Lee <leedm777@yahoo.com>
2021-12-10 21:14:31 -0600

    Cleaned up day 8
    Toward the end of my soln. I realized it would be much cleaner to
    represent the segments as bits instead of keeping them as letters.
    Cleaned up the code to just encode it as bits from the beginning.

--- src/day08.rs
+++ src/day08.rs
@@ -1,20 +1,55 @@
 use std::collections::HashMap;
 
+#[derive(Clone, Eq, PartialEq, Debug)]
+pub struct Pattern {
+    segments: u8,
+    num_bits: u8,
+}
+
+fn encode(s: &str) -> u8 {
+    s.chars()
+        .map(|ch| ch as i32 - 'a' as i32)
+        .fold(0, |a, b| a | (1 << b))
+}
+
+fn parse_pattern(s: &str) -> Pattern {
+    let segments = encode(s);
+    let bits = s.len() as u8;
+    Pattern {
+        segments,
+        num_bits: bits,
+    }
+}
+
 #[derive(Eq, PartialEq, Debug)]
 pub struct Entry {
-    patterns: Vec<String>,
-    output: [String; 4],
+    patterns: [Pattern; 10],
+    output: [Pattern; 4],
 }
 
 fn parse_entry(line: &str) -> Entry {
     let (patterns, output) = line.split_once(" | ").expect("Could not parse line");
-    let patterns = patterns.split_whitespace().map(|s| s.to_string()).collect();
-    let mut output = output.split_whitespace();
+
+    let mut patterns = patterns.split_whitespace().map(parse_pattern);
+    let patterns = [
+        patterns.next().expect("Could not parse pattern"),
+        patterns.next().expect("Could not parse pattern"),
+        patterns.next().expect("Could not parse pattern"),
+        patterns.next().expect("Could not parse pattern"),
+        patterns.next().expect("Could not parse pattern"),
+        patterns.next().expect("Could not parse pattern"),
+        patterns.next().expect("Could not parse pattern"),
+        patterns.next().expect("Could not parse pattern"),
+        patterns.next().expect("Could not parse pattern"),
+        patterns.next().expect("Could not parse pattern"),
+    ];
+
+    let mut output = output.split_whitespace().map(parse_pattern);
     let output = [
-        output.next().expect("Could not parse output").to_string(),
-        output.next().expect("Could not parse output").to_string(),
-        output.next().expect("Could not parse output").to_string(),
-        output.next().expect("Could not parse output").to_string(),
+        output.next().expect("Could not parse output"),
+        output.next().expect("Could not parse output"),
+        output.next().expect("Could not parse output"),
+        output.next().expect("Could not parse output"),
     ];
 
     return Entry { patterns, output };
@@ -28,74 +63,46 @@ pub fn part1(input: &Vec<Entry>) -> i32 {
     let matches = input
         .iter()
         .flat_map(|e| e.output.to_vec())
-        .filter(|s| [2, 3, 4, 7].contains(&s.len()));
+        .filter(|p| [2, 3, 4, 7].contains(&p.num_bits));
     // matches.clone().for_each(|s| println!("{}", s));
     return matches.count() as i32;
-}
-
-// const BASE_PATTERNS: [&str; 10] = [
-//     "abcefg",  // 0
-//     "cf",      // 1
-//     "acdeg",   // 2
-//     "acdfg",   // 3
-//     "bcdf",    // 4
-//     "abdfg",   // 5
-//     "abdefg",  // 6
-//     "acf",     // 7
-//     "abcdefg", // 8
-//     "abcdfg",  // 9
-// ];
-
-/*
- * Segment counts:
- *  e: 4 - unique
- *  b: 6 - unique
- *  d: 7
- *  g: 7
- *  a: 8
- *  c: 8
- *  f: 9 - unique
- */
-
-fn encode(s: &str) -> i32 {
-    s.chars()
-        .map(|ch| ch as i32 - 'a' as i32)
-        .fold(0, |a, b| a | (1 << b))
 }
 
 fn solve(entry: &Entry) -> i32 {
     let one = entry
         .patterns
         .iter()
-        .find(|s| s.len() == 2)
+        .find(|p| p.num_bits == 2)
         .expect("Could not find 1");
     let seven = entry
         .patterns
         .iter()
-        .find(|s| s.len() == 3)
+        .find(|p| p.num_bits == 3)
         .expect("Could not find 7");
     let four = entry
         .patterns
         .iter()
-        .find(|s| s.len() == 4)
+        .find(|p| p.num_bits == 4)
         .expect("Could not find 4");
     let eight = entry
         .patterns
         .iter()
-        .find(|s| s.len() == 7)
+        .find(|p| p.num_bits == 7)
         .expect("Could not find 8");
 
-    let a = seven
-        .chars()
-        .find(|ch| one.chars().nth(0).unwrap() != *ch && one.chars().nth(1).unwrap() != *ch)
-        .expect("Could not find a");
+    let a = seven.segments & !one.segments;
 
-    let mut e = '\0';
-    let mut f = '\0';
-    let mut b = '\0';
+    let mut e = 0u8;
+    let mut f = 0u8;
+    let mut b = 0u8;
 
-    for ch in 'a'..='g' {
-        let count = entry.patterns.iter().filter(|p| p.contains(ch)).count();
+    for ch in 0..=7 {
+        let ch = 1 << ch;
+        let count = entry
+            .patterns
+            .iter()
+            .filter(|p| (p.segments & ch) != 0u8)
+            .count();
         if count == 4 {
             e = ch;
         } else if count == 6 {
@@ -105,42 +112,26 @@ fn solve(entry: &Entry) -> i32 {
         }
     }
 
-    let c = one.chars().find(|c| *c != f).expect("Could not find c");
-    let d = four
-        .chars()
-        .find(|ch| ![b, c, f].contains(ch))
-        .expect("Could not find d");
-    let g = ('a'..='g')
-        .find(|ch| ![a, b, c, d, e, f].contains(ch))
-        .expect("Could not find g");
+    let c = one.segments & !f;
+    let d = four.segments & !(b | c | f);
+    let g = 0b111_1111 & !(a | b | c | d | e | f);
 
-    let zero = encode(&[a, b, c, e, f, g].iter().collect::<String>());
-    let one = encode(one);
-    let two = encode(&[a, c, d, e, g].iter().collect::<String>());
-    let three = encode(&[a, c, d, f, g].iter().collect::<String>());
-    let four = encode(four);
-    let five = encode(&[a, b, d, f, g].iter().collect::<String>());
-    let six = encode(&[a, b, d, e, f, g].iter().collect::<String>());
-    let seven = encode(seven);
-    let eight = encode(eight);
-    let nine = encode(&[a, b, c, d, f, g].iter().collect::<String>());
-
-    let mut m = HashMap::new();
-    m.insert(zero, 0);
-    m.insert(one, 1);
-    m.insert(two, 2);
-    m.insert(three, 3);
-    m.insert(four, 4);
-    m.insert(five, 5);
-    m.insert(six, 6);
-    m.insert(seven, 7);
-    m.insert(eight, 8);
-    m.insert(nine, 9);
+    let mut m: HashMap<u8, i32> = HashMap::new();
+    m.insert(a | b | c | e | f | g, 0);
+    m.insert(one.segments, 1);
+    m.insert(a | c | d | e | g, 2);
+    m.insert(a | c | d | f | g, 3);
+    m.insert(four.segments, 4);
+    m.insert(a | b | d | f | g, 5);
+    m.insert(a | b | d | e | f | g, 6);
+    m.insert(seven.segments, 7);
+    m.insert(eight.segments, 8);
+    m.insert(a | b | c | d | f | g, 9);
 
     let mut r = 0;
     for i in 0..4 {
         let v = m
-            .get(&encode(&entry.output[i]))
+            .get(&entry.output[i].segments)
             .expect("Could not decode output");
         r = r * 10 + v;
     }
@@ -183,23 +174,6 @@ mod tests {
         return util::read_input(8);
     }
 
-    #[test]
-    fn test_parse_entry() {
-        let actual = parse_entry("be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe");
-        assert_eq!(
-            actual,
-            Entry {
-                patterns: vec![
-                    "be", "cfbegad", "cbdgef", "fgaecd", "cgeb", "fdcge", "agebfd", "fecdb",
-                    "fabcd", "edb"
-                ]
-                .iter()
-                .map(|s| s.to_string())
-                .collect(),
-                output: ["fdgacbe", "cefdb", "cefbgd", "gcbe"].map(|s| s.to_string()),
-            }
-        )
-    }
     #[test]
     fn test_part1_ex1() {
         let actual = part1(&parse(&ex1()));
