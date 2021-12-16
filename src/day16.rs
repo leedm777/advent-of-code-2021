ab4a2c36a4a332a7252aa31d4aa184a6cc75ef08
David M. Lee <leedm777@yahoo.com>
2021-12-16 12:09:39 -0600

    Day 16 - char.to_digit() to the rescue!

--- src/day16.rs
+++ src/day16.rs
@@ -108,25 +108,7 @@ impl Bits {
         let mut data = vec![];
         data.reserve(input.len() * 4);
         for ch in input.trim().chars() {
-            let v = match ch {
-                '0' => 0,
-                '1' => 1,
-                '2' => 2,
-                '3' => 3,
-                '4' => 4,
-                '5' => 5,
-                '6' => 6,
-                '7' => 7,
-                '8' => 8,
-                '9' => 9,
-                'A' => 10,
-                'B' => 11,
-                'C' => 12,
-                'D' => 13,
-                'E' => 14,
-                'F' => 15,
-                _ => panic!("Invalid hex char"),
-            };
+            let v = ch.to_digit(16).expect("Invalid hex digit");
             for shift in (0..4).rev() {
                 data.push((v & (1 << shift)) != 0);
             }
