bd73a411927954c28f2189c1ea56585dfae16605
David M. Lee <leedm777@yahoo.com>
2021-12-27 01:34:18 -0600

    Day 24, making it worse

--- src/day24.rs
+++ src/day24.rs
@@ -1,28 +1,29 @@
 use regex::Regex;
 use std::collections::HashMap;
 
-#[derive(Clone, Debug)]
-enum Op {
-    Add,
-    Mul,
-    Div,
-    Mod,
-    Eql,
-}
+// TODO: Try removing all the lifetimes and just use boxes instead
 
 #[derive(Clone, Debug)]
-enum Expression {
+enum Expression<'a> {
     Constant(i64),
     Input(u8),
-    Calculation(Op, Box<Expression>, Box<Expression>),
+    Add(&'a Expression<'a>, &'a Expression<'a>),
+    Mul(&'a Expression<'a>, &'a Expression<'a>),
+    Div(&'a Expression<'a>, &'a Expression<'a>),
+    Mod(&'a Expression<'a>, &'a Expression<'a>),
+    Eql(&'a Expression<'a>, &'a Expression<'a>),
 }
 
-impl ToString for Expression {
+impl ToString for Expression<'_> {
     fn to_string(&self) -> String {
         match self {
-            Expression::Constant(c) => format!("{}", c),
+            Expression::Constant(c) => c.to_string(),
             Expression::Input(i) => format!("i{}", i),
-            Expression::Calculation(op, lhs, rhs) => format!("({:?} {:?} {:?})", op, lhs, rhs),
+            Expression::Add(lhs, rhs) => format!("({}) _ ({})", lhs.to_string(), rhs.to_string()),
+            Expression::Mul(lhs, rhs) => format!("({}) _ ({})", lhs.to_string(), rhs.to_string()),
+            Expression::Div(lhs, rhs) => format!("({}) _ ({})", lhs.to_string(), rhs.to_string()),
+            Expression::Mod(lhs, rhs) => format!("({}) _ ({})", lhs.to_string(), rhs.to_string()),
+            Expression::Eql(lhs, rhs) => format!("({}) _ ({})", lhs.to_string(), rhs.to_string()),
         }
     }
 }
@@ -31,22 +32,6 @@ impl ToString for Expression {
 enum Value {
     Literal(i64),
     Variable(char),
-}
-
-impl Value {
-    fn get(&self, alu: &ALU) -> i64 {
-        match self {
-            Value::Literal(v) => *v,
-            Value::Variable(v) => alu.get(*v),
-        }
-    }
-
-    fn meta_get(&self, alu: &ALU) -> Expression {
-        match self {
-            Value::Literal(v) => Expression::Constant(*v),
-            Value::Variable(v) => alu.meta_get(*v).clone(),
-        }
-    }
 }
 
 #[derive(Clone, Debug)]
@@ -95,246 +80,78 @@ impl Operation {
         }
     }
 
-    fn exec(&self, alu: &mut ALU) {
+    fn exec<'a>(&self, alu: &'a mut ALU<'a>) {
         match self {
             Operation::Inp(ch) => {
-                let v = alu.input.pop().expect("End of input");
-                alu.set(*ch, v);
+                alu.input(*ch);
             }
-            Operation::Add(ch, v) => {
-                let lhs = alu.get(*ch);
-                let rhs = v.get(alu);
-                alu.set(*ch, lhs + rhs);
-            }
-            Operation::Mul(ch, v) => {
-                let lhs = alu.get(*ch);
-                let rhs = v.get(alu);
-                alu.set(*ch, lhs * rhs);
-            }
-            Operation::Div(ch, v) => {
-                let lhs = alu.get(*ch);
-                let rhs = v.get(alu);
-                alu.set(*ch, lhs / rhs);
-            }
-            Operation::Mod(ch, v) => {
-                let lhs = alu.get(*ch);
-                let rhs = v.get(alu);
-                alu.set(*ch, lhs % rhs);
-            }
-            Operation::Eql(ch, v) => {
-                let lhs = alu.get(*ch);
-                let rhs = v.get(alu);
-                let res = if lhs == rhs { 1 } else { 0 };
-                alu.set(*ch, res);
-            }
+            Operation::Add(ch, v) => {}
+            Operation::Mul(ch, v) => {}
+            Operation::Div(ch, v) => {}
+            Operation::Mod(ch, v) => {}
+            Operation::Eql(ch, v) => {}
         }
-    }
 
-    fn meta_exec(&self, alu: &mut ALU) {
-        println!("{:?}", self);
-        match self {
-            Operation::Inp(ch) => {
-                let i = alu.next_input;
-                alu.next_input += 1;
-
-                let v = Expression::Input(i);
-                alu.meta_set(*ch, v);
-            }
-            Operation::Add(ch, v) => {
-                let lhs = alu.meta_get(*ch).clone();
-                let rhs = v.meta_get(alu);
-                alu.meta_set(
-                    *ch,
-                    Expression::Calculation(Op::Add, Box::new(lhs), Box::new(rhs)),
-                );
-            }
-            Operation::Mul(ch, v) => {
-                let lhs = alu.meta_get(*ch).clone();
-                let rhs = v.meta_get(alu);
-                alu.meta_set(
-                    *ch,
-                    Expression::Calculation(Op::Mul, Box::new(lhs), Box::new(rhs)),
-                );
-            }
-            Operation::Div(ch, v) => {
-                let lhs = alu.meta_get(*ch).clone();
-                let rhs = v.meta_get(alu);
-                alu.meta_set(
-                    *ch,
-                    Expression::Calculation(Op::Div, Box::new(lhs), Box::new(rhs)),
-                );
-            }
-            Operation::Mod(ch, v) => {
-                let lhs = alu.meta_get(*ch).clone();
-                let rhs = v.meta_get(alu);
-                alu.meta_set(
-                    *ch,
-                    Expression::Calculation(Op::Mod, Box::new(lhs), Box::new(rhs)),
-                );
-            }
-            Operation::Eql(ch, v) => {
-                let lhs = alu.meta_get(*ch).clone();
-                let rhs = v.meta_get(alu);
-                alu.meta_set(
-                    *ch,
-                    Expression::Calculation(Op::Eql, Box::new(lhs), Box::new(rhs)),
-                );
-            }
-        }
+        unimplemented!("TODO!")
     }
 }
 
-#[derive(Clone)]
-pub struct ALU {
+#[derive(Clone, Debug)]
+pub struct ALU<'a> {
     program: Vec<Operation>,
-    memory: HashMap<char, i64>,
-    input: Vec<i64>,
-
-    meta_memory: HashMap<char, Expression>,
+    memory: HashMap<char, &'a Expression<'a>>,
     next_input: u8,
+
+    // memory management
+    expressions: Vec<Expression<'a>>,
 }
 
-impl ALU {
-    fn get(&self, ch: char) -> i64 {
-        *self.memory.get(&ch).unwrap_or(&0)
-    }
-
-    fn set(&mut self, ch: char, v: i64) {
-        self.memory.insert(ch, v);
-    }
-
-    fn meta_get(&self, ch: char) -> &Expression {
-        self.meta_memory.get(&ch).expect("Unexpected variable")
-    }
-
-    fn meta_set(&mut self, ch: char, v: Expression) {
-        self.meta_memory.insert(ch, v);
-    }
-
-    fn run(&mut self) {
+impl<'a> ALU<'a> {
+    fn run(&'a mut self) {
         while let Some(op) = self.program.pop() {
             op.exec(self);
         }
     }
 
-    fn meta_run(&mut self) {
-        while let Some(op) = self.program.pop() {
-            op.meta_exec(self);
+    fn get(&mut self, ch: char) -> &Expression<'_> {
+        match self.memory.get(&ch) {
+            Some(exp) => exp,
+            None => {
+                let exp = Expression::Constant(0);
+                self.expressions.push(exp);
+                self.expressions.last().expect("But I just pushed it there")
+            }
         }
     }
 
-    fn run_input(&self, input: &Vec<i64>) -> Self {
-        let mut alu = self.clone();
-        alu.input = input.clone();
-        alu.run();
-        alu
+    fn set(&mut self, ch: char, exp: &'a Expression<'a>) {
+        self.memory.insert(ch, exp);
+    }
+
+    fn input<'b>(&'a mut self, ch: char) {
+        let exp = Expression::Input(self.next_input);
+        self.next_input += 1;
+        self.expressions.push(exp);
+        let exp = self.expressions.last().expect("But I just pushed it");
+
+        self.set(ch, exp);
     }
 }
 
-pub fn parse(input: &str) -> ALU {
-    let mut meta_memory = HashMap::new();
-    meta_memory.insert('w', Expression::Constant(0));
-    meta_memory.insert('x', Expression::Constant(0));
-    meta_memory.insert('y', Expression::Constant(0));
-    meta_memory.insert('z', Expression::Constant(0));
-
+pub fn parse<'a, 'b>(input: &'a str) -> ALU<'b> {
     return ALU {
         program: input.lines().map(Operation::parse).rev().collect(),
         memory: HashMap::new(),
-        input: vec![],
-
-        meta_memory,
         next_input: 0,
+        expressions: vec![],
     };
 }
 
 pub fn part1(init: &ALU) -> i64 {
     let mut meta = init.clone();
-    meta.meta_run();
+    meta.run();
 
-    println!("{:?}", meta.meta_get('z'));
-
-    /*
-    let mut input = vec![1i64; 14];
-    let mut best = input.clone();
-    let mut min_z = i64::MAX;
-
-    // find the best we can with single digits
-    loop {
-        for digit in 0..14 {
-            let mut min_digit = 0i64;
-            for i in 1..=9 {
-                input[digit] = i;
-                let next = init.run_input(&input);
-                let z = next.get('z');
-                if z == 0 {
-                    return input_to_i64(&input);
-                }
-                if z <= min_z {
-                    min_z = z;
-                    min_digit = i;
-                }
-            }
-            input[digit] = min_digit;
-            let next = init.run_input(&input);
-            println!("{} => {}", input_to_i64(&input), next.get('z'));
-        }
-        println!();
-        if best == input {
-            break;
-        }
-        best = input.clone();
-    }
-
-    // double digits
-    loop {
-        for digit1 in 0..14 {
-            let mut min_digit1 = input[digit1];
-
-            for i in 1..=9 {
-                input[digit1] = i;
-
-                for digit2 in 0..14 {
-                    if digit1 == digit2 {
-                        continue;
-                    }
-
-                    let mut min_digit2 = input[digit2];
-                    for j in 1..=9 {
-                        input[digit2] = j;
-                        let next = init.run_input(&input);
-                        let z = next.get('z');
-                        if z == 0 {
-                            return input_to_i64(&input);
-                        }
-                        if z <= min_z {
-                            min_z = z;
-                            min_digit1 = i;
-                            min_digit2 = j;
-                        }
-                    }
-                    assert_ne!(min_digit2, 0);
-                    input[digit2] = min_digit2;
-                }
-            }
-
-            assert_ne!(min_digit1, 0);
-            input[digit1] = min_digit1;
-
-            let next = init.run_input(&input);
-            println!("{} => {}", input_to_i64(&input), next.get('z'));
-        }
-        println!();
-
-        // didn't find anything better; break
-        if best == input {
-            break;
-        }
-
-        // slight improvement; keep trying
-        best = input.clone();
-    }
-     */
+    println!("{:?}", meta.get('z'));
 
     -1
 }
@@ -359,9 +176,8 @@ mod tests {
     #[test]
     fn test_ex1() {
         let mut alu = parse(&ex1());
-        alu.input = vec![314];
         alu.run();
-        assert_eq!(alu.get('x'), -314);
+        assert_eq!(alu.get('x').to_string(), "(i0) * (-1)");
     }
 
     fn ex2() -> String {
@@ -375,12 +191,11 @@ mod tests {
     #[test]
     fn test_ex2() {
         let mut alu = parse(&ex2());
-        alu.input = vec![10];
         alu.run();
-        assert_eq!(alu.get('w'), 1);
-        assert_eq!(alu.get('x'), 0);
-        assert_eq!(alu.get('y'), 1);
-        assert_eq!(alu.get('z'), 0);
+        assert_eq!(alu.get('w').to_string(), "");
+        assert_eq!(alu.get('x').to_string(), "");
+        assert_eq!(alu.get('y').to_string(), "");
+        assert_eq!(alu.get('z').to_string(), "");
     }
 
     fn real() -> String {
