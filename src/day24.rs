f8dae6e49d1b81882c61e00e9e042a88366997df
David M. Lee <leedm777@yahoo.com>
2021-12-27 22:57:40 -0600

    Day 24, expressions, but too slow

--- src/day24.rs
+++ src/day24.rs
@@ -1,29 +1,27 @@
 use regex::Regex;
 use std::collections::HashMap;
 
-// TODO: Try removing all the lifetimes and just use boxes instead
-
 #[derive(Clone, Debug)]
-enum Expression<'a> {
+enum Expression {
     Constant(i64),
     Input(u8),
-    Add(&'a Expression<'a>, &'a Expression<'a>),
-    Mul(&'a Expression<'a>, &'a Expression<'a>),
-    Div(&'a Expression<'a>, &'a Expression<'a>),
-    Mod(&'a Expression<'a>, &'a Expression<'a>),
-    Eql(&'a Expression<'a>, &'a Expression<'a>),
+    Add(Box<Expression>, Box<Expression>),
+    Mul(Box<Expression>, Box<Expression>),
+    Div(Box<Expression>, Box<Expression>),
+    Mod(Box<Expression>, Box<Expression>),
+    Eql(Box<Expression>, Box<Expression>),
 }
 
-impl ToString for Expression<'_> {
+impl ToString for Expression {
     fn to_string(&self) -> String {
         match self {
             Expression::Constant(c) => c.to_string(),
             Expression::Input(i) => format!("i{}", i),
-            Expression::Add(lhs, rhs) => format!("({}) _ ({})", lhs.to_string(), rhs.to_string()),
-            Expression::Mul(lhs, rhs) => format!("({}) _ ({})", lhs.to_string(), rhs.to_string()),
-            Expression::Div(lhs, rhs) => format!("({}) _ ({})", lhs.to_string(), rhs.to_string()),
-            Expression::Mod(lhs, rhs) => format!("({}) _ ({})", lhs.to_string(), rhs.to_string()),
-            Expression::Eql(lhs, rhs) => format!("({}) _ ({})", lhs.to_string(), rhs.to_string()),
+            Expression::Add(lhs, rhs) => format!("{} + {}", lhs.to_string(), rhs.to_string()),
+            Expression::Mul(lhs, rhs) => format!("{} * {}", lhs.to_string(), rhs.to_string()),
+            Expression::Div(lhs, rhs) => format!("{} / {}", lhs.to_string(), rhs.to_string()),
+            Expression::Mod(lhs, rhs) => format!("{} % {}", lhs.to_string(), rhs.to_string()),
+            Expression::Eql(lhs, rhs) => format!("{} == {}", lhs.to_string(), rhs.to_string()),
         }
     }
 }
@@ -80,70 +78,196 @@ impl Operation {
         }
     }
 
-    fn exec<'a>(&self, alu: &'a mut ALU<'a>) {
+    fn exec(&self, alu: &mut ALU) {
+        println!("({}) {:?}", alu.next_input, self);
         match self {
             Operation::Inp(ch) => {
-                alu.input(*ch);
+                let exp = Expression::Input(alu.next_input);
+                alu.next_input += 1;
+                alu.set(*ch, exp);
             }
-            Operation::Add(ch, v) => {}
-            Operation::Mul(ch, v) => {}
-            Operation::Div(ch, v) => {}
-            Operation::Mod(ch, v) => {}
-            Operation::Eql(ch, v) => {}
-        }
+            Operation::Add(ch, v) => {
+                // replace what's on the left hand side to make the borrow checker happy
+                let lhs = std::mem::replace(
+                    alu.memory
+                        .entry(*ch)
+                        .or_insert_with(|| Expression::Constant(0)),
+                    Expression::Constant(0),
+                );
+                let rhs = match v {
+                    Value::Literal(i) => alu
+                        .constants
+                        .entry(*i)
+                        .or_insert_with(|| Expression::Constant(*i)),
+                    Value::Variable(ch) => alu
+                        .memory
+                        .entry(*ch)
+                        .or_insert_with(|| Expression::Constant(0)),
+                };
 
-        unimplemented!("TODO!")
+                let exp = match (lhs, rhs) {
+                    (Expression::Constant(lhs), Expression::Constant(rhs)) => {
+                        Expression::Constant(lhs + *rhs)
+                    }
+                    (lhs, rhs) => Expression::Add(Box::new(lhs), Box::new(rhs.clone())),
+                };
+                alu.set(*ch, exp);
+            }
+            Operation::Mul(ch, v) => {
+                // replace what's on the left hand side to make the borrow checker happy
+                let lhs = std::mem::replace(
+                    alu.memory
+                        .entry(*ch)
+                        .or_insert_with(|| Expression::Constant(0)),
+                    Expression::Constant(0),
+                );
+                let rhs = match v {
+                    Value::Literal(i) => alu
+                        .constants
+                        .entry(*i)
+                        .or_insert_with(|| Expression::Constant(*i)),
+                    Value::Variable(ch) => alu
+                        .memory
+                        .entry(*ch)
+                        .or_insert_with(|| Expression::Constant(0)),
+                };
+
+                let exp = match (lhs, rhs) {
+                    (Expression::Constant(lhs), Expression::Constant(rhs)) => {
+                        Expression::Constant(lhs * *rhs)
+                    }
+                    (lhs, rhs) => Expression::Mul(Box::new(lhs), Box::new(rhs.clone())),
+                };
+                alu.set(*ch, exp);
+            }
+            Operation::Div(ch, v) => {
+                // replace what's on the left hand side to make the borrow checker happy
+                let lhs = std::mem::replace(
+                    alu.memory
+                        .entry(*ch)
+                        .or_insert_with(|| Expression::Constant(0)),
+                    Expression::Constant(0),
+                );
+                let rhs = match v {
+                    Value::Literal(i) => alu
+                        .constants
+                        .entry(*i)
+                        .or_insert_with(|| Expression::Constant(*i)),
+                    Value::Variable(ch) => alu
+                        .memory
+                        .entry(*ch)
+                        .or_insert_with(|| Expression::Constant(0)),
+                };
+
+                let exp = match (lhs, rhs) {
+                    (Expression::Constant(lhs), Expression::Constant(rhs)) => {
+                        Expression::Constant(lhs / *rhs)
+                    }
+                    (lhs, rhs) => Expression::Div(Box::new(lhs), Box::new(rhs.clone())),
+                };
+                alu.set(*ch, exp);
+            }
+            Operation::Mod(ch, v) => {
+                // replace what's on the left hand side to make the borrow checker happy
+                let lhs = std::mem::replace(
+                    alu.memory
+                        .entry(*ch)
+                        .or_insert_with(|| Expression::Constant(0)),
+                    Expression::Constant(0),
+                );
+                let rhs = match v {
+                    Value::Literal(i) => alu
+                        .constants
+                        .entry(*i)
+                        .or_insert_with(|| Expression::Constant(*i)),
+                    Value::Variable(ch) => alu
+                        .memory
+                        .entry(*ch)
+                        .or_insert_with(|| Expression::Constant(0)),
+                };
+
+                let exp = match (lhs, rhs) {
+                    (Expression::Constant(lhs), Expression::Constant(rhs)) => {
+                        Expression::Constant(lhs % *rhs)
+                    }
+                    (lhs, rhs) => Expression::Mod(Box::new(lhs), Box::new(rhs.clone())),
+                };
+                alu.set(*ch, exp);
+            }
+            Operation::Eql(ch, v) => {
+                // replace what's on the left hand side to make the borrow checker happy
+                let lhs = std::mem::replace(
+                    alu.memory
+                        .entry(*ch)
+                        .or_insert_with(|| Expression::Constant(0)),
+                    Expression::Constant(0),
+                );
+                let rhs = match v {
+                    Value::Literal(i) => alu
+                        .constants
+                        .entry(*i)
+                        .or_insert_with(|| Expression::Constant(*i)),
+                    Value::Variable(ch) => alu
+                        .memory
+                        .entry(*ch)
+                        .or_insert_with(|| Expression::Constant(0)),
+                };
+
+                let exp = match (lhs, rhs) {
+                    (Expression::Constant(lhs), Expression::Constant(rhs)) => {
+                        Expression::Constant(if lhs == *rhs { 1 } else { 0 })
+                    }
+                    (Expression::Input(inp), Expression::Constant(rhs)) => {
+                        if *rhs < 1 || *rhs > 9 {
+                            Expression::Constant(0)
+                        } else {
+                            Expression::Eql(
+                                Box::new(Expression::Input(inp)),
+                                Box::new(Expression::Constant(*rhs)),
+                            )
+                        }
+                    }
+                    (lhs, rhs) => Expression::Eql(Box::new(lhs), Box::new(rhs.clone())),
+                };
+                alu.set(*ch, exp);
+            }
+        }
     }
 }
 
 #[derive(Clone, Debug)]
-pub struct ALU<'a> {
+pub struct ALU {
     program: Vec<Operation>,
-    memory: HashMap<char, &'a Expression<'a>>,
+    memory: HashMap<char, Expression>,
     next_input: u8,
 
-    // memory management
-    expressions: Vec<Expression<'a>>,
+    constants: HashMap<i64, Expression>,
 }
 
-impl<'a> ALU<'a> {
-    fn run(&'a mut self) {
+impl ALU {
+    fn run(&mut self) {
         while let Some(op) = self.program.pop() {
             op.exec(self);
         }
     }
 
-    fn get(&mut self, ch: char) -> &Expression<'_> {
-        match self.memory.get(&ch) {
-            Some(exp) => exp,
-            None => {
-                let exp = Expression::Constant(0);
-                self.expressions.push(exp);
-                self.expressions.last().expect("But I just pushed it there")
-            }
-        }
+    fn get(&mut self, ch: char) -> &Expression {
+        self.memory
+            .entry(ch)
+            .or_insert_with(|| Expression::Constant(0))
     }
-
-    fn set(&mut self, ch: char, exp: &'a Expression<'a>) {
+    fn set(&mut self, ch: char, exp: Expression) {
         self.memory.insert(ch, exp);
-    }
-
-    fn input<'b>(&'a mut self, ch: char) {
-        let exp = Expression::Input(self.next_input);
-        self.next_input += 1;
-        self.expressions.push(exp);
-        let exp = self.expressions.last().expect("But I just pushed it");
-
-        self.set(ch, exp);
     }
 }
 
-pub fn parse<'a, 'b>(input: &'a str) -> ALU<'b> {
+pub fn parse(input: &str) -> ALU {
     return ALU {
         program: input.lines().map(Operation::parse).rev().collect(),
         memory: HashMap::new(),
         next_input: 0,
-        expressions: vec![],
+
+        constants: HashMap::new(),
     };
 }
 
@@ -151,7 +275,13 @@ pub fn part1(init: &ALU) -> i64 {
     let mut meta = init.clone();
     meta.run();
 
-    println!("{:?}", meta.get('z'));
+    let ch = 'z';
+    println!(
+        "{:?}",
+        meta.memory
+            .entry(ch)
+            .or_insert_with(|| Expression::Constant(0))
+    );
 
     -1
 }
